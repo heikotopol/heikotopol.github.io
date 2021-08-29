a0e3e8c63f9069fa0b92222d6c18ab1315a93d79
heikotopol <89635542+heikotopol@users.noreply.github.com>
2021-08-29 21:51:20 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,8 +5,23 @@ var addressPoints = [
     8.4425103
   ],
   [
+    "Bologna, Italy",
+    44.5121449,
+     11.3605547
+  ],
+  [
+    "Castro Urdiales, Spain",
+    43.3767236,
+    -3.2147719
+  ],
+  [
     "Doha, Qatar",
     25.3164718,
     51.4383021
-  ]
+  ],
+  [
+    "Dnirpo, Ukraine",
+    48.453269,
+    35.0517494
+  ],
 ];
