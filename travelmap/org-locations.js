ecd382ff0aded16b9995810165b3e537b92e4a44
heikotopol <89635542+heikotopol@users.noreply.github.com>
2021-08-30 10:10:11 +0200

    Add files via upload

---
+++ travelmap/org-locations.js
@@ -0,0 +1,87 @@
+var addressPoints = [
+  [
+    "RWTH Aachen, Germany",
+    50.7777202,
+     6.0797021
+  ],
+   [
+    "Bad Herrenalb, Germany",
+    48.800517,
+    8.4425103
+  ],
+  [
+    "Beijing, China",
+    39.953288,
+    116.3451314
+  ],
+  [
+    "Bochum, Germany",
+    51.4456692,
+    7.2594206
+  ],
+  [
+    "Bologna, Italy",
+    44.5121449,
+    11.3605547
+  ],
+  [
+    "Castro Urdiales, Spain",
+    43.3767236,
+    -3.2147719
+  ],
+  [
+    "Center for Advanced Materials, Qatar",
+    25.3782243,
+    51.4891378
+  ],
+  [
+    "Chalkidiki, Greece",
+    40.0601822,
+    23.4620749
+  ],
+  [
+    "College Station, TX",
+    30.6196709,
+    -96.3407719
+  ],
+  [
+    "Doha, Qatar",
+    25.3164718,
+    51.4383021
+  ],
+  [
+    "Dnirpo, Ukraine",
+    48.4532690,
+    35.0495607
+  ],
+  [
+    "East Lansing, MI",
+    42.7311558,
+    -84.4930818
+  ],
+  [
+    "Erlangen, Germany",
+    49.57368,
+    11.0284113
+  ],
+  [
+    "Lecce, Italy",
+    40.3612492,
+    18.1666645
+  ],
+  [
+    "Nottingham, UK",
+    52.9397827,
+    -1.1951883
+  ],
+  [
+    "Rhodes, Greece",
+    36.4239315,
+    28.1966502
+  ],
+  [
+    "Rosemont, IL",
+    41.9812432,
+    -87.8611541
+  ]
+];
